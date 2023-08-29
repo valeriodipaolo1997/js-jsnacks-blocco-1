6fc8c2682fcd025f5e2d0a570d4024745bd14af3
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-08-29 15:27:21 +0200

    Snack1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,3 +1,16 @@
 /*L’utente inserisce due numeri in successione, con due prompt.
 Il software stampa il maggiore.*/
 
+
+//definisco le variabili
+const firstNumber = Number(prompt('Type first number'));
+const secondNumber = Number(prompt('Type second number'));
+const numberEl = document.querySelector('.number');
+
+
+//controllo se il primo numero è piu grande e viceversa
+if (firstNumber > secondNumber) {
+    numberEl.innerHTML = firstNumber
+} else if (secondNumber > firstNumber || secondNumber === firstNumber) {
+    numberEl.innerHTML = secondNumber
+}
