5670054421971dceafd2c47d625b453fdf58a83d
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-08-30 11:54:52 +0200

    add ciclo while

--- jsnack1/jsnack2/script.js
+++ jsnack1/jsnack2/script.js
@@ -3,7 +3,7 @@ Il software stampa prima la parola più corta, poi la parola più lunga.*/
 
 
 //definisco le variabili
-const firstWord = prompt('Type first word');
+/*const firstWord = prompt('Type first word');
 
 const secondWord = prompt('Type second word');
 
@@ -25,4 +25,24 @@ if (lengthFirstWord > lengthSecondWord) {
     wordElement.insertAdjacentHTML('beforeend', `${firstWord}  `);
     wordElement.insertAdjacentHTML('beforeend', `${secondWord}  `);
 
+}
+*/
+
+
+
+let i = 0;
+let listwords = []
+
+while (i < 2) {
+    const word = prompt("type a word");
+    listwords.push(word)
+    i++
+}
+
+if (listwords[0] > listwords[1]) {
+    console.log(listwords[1], listwords[0]);
+} else if (listwords[0] < listwords[1]) {
+    console.log(listwords[0], listwords[1]);
+} else {
+    console.log(listwords[0], listwords[1]);
 }
