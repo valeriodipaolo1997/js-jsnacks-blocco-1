c8b8cb48682b0e92c4e6d5fde50bbe3728b4239f
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-08-30 11:00:35 +0200

    add ciclo while

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -7,10 +7,32 @@ const firstNumber = Number(prompt('Type first number'));
 const secondNumber = Number(prompt('Type second number'));
 const numberEl = document.querySelector('.number');
 
-
+/*
 //controllo se il primo numero è piu grande e viceversa
 if (firstNumber > secondNumber) {
     numberEl.innerHTML = firstNumber
 } else if (secondNumber > firstNumber || secondNumber === firstNumber) {
     numberEl.innerHTML = secondNumber
+}*/
+
+
+let isNaNcheck = false;
+
+while (isNaNcheck === false) {
+
+    if (firstNumber > secondNumber) {
+
+        numberEl.innerHTML = `${firstNumber} numero piu grande`
+
+    } else if (secondNumber > firstNumber) {
+
+        numberEl.innerHTML = `${secondNumber} numero piu grande`
+
+    } else if (isNaNcheck === false) {
+
+        numberEl.innerHTML = `${firstNumber} e ${secondNumber} sono uguali`
+
+    }
+
+    isNaNcheck = true
 }
